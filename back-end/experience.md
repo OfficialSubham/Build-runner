a67024bfa217e0af4876fcbcf6b0d373b65d773e
Subham Mondal <subhamoffi@gmail.com>
2026-06-04 15:30:39 +0530

    Successfully created the build runnder backend

---
+++ back-end/experience.md
@@ -0,0 +1,23 @@
+- 04/06/26
+
+Today I made the build runner up until where user put a zip file and i extract it in my backend and run their project
+
+Here is the flow
+
+First user give a zip file
+then i check for weather that zip file is valid or not
+
+Then I generate a deployment Id which would be the name of the folder where i will put that zip file
+
+After that I create some folder along with the folder with the deployment Id inside the deployments folder
+Inside the deployment Id folder I then write the zip file
+then extract the zip file inside the source folder under that deploymentId folder
+
+I also imported exec which will run some command in the terminal which i would ask him to run.
+I also convert it into a promisify through the util lib
+cause raw exec uses callback which is too ugly and difficult to understand
+then
+I ran npm install inside the source folder of the extracted zip file user had given me
+
+Then created a child process cause when i asked it why can't i just run the npm run dev in the execpromise it was saying that if i do that then it will not return from the promise as it is a long running process and the browser will not get any response so
+we created a spawn a child which create a another terminal let's say and run the npm run dev inside that folder then it is successfully running the user's project
--- back-end/index.ts
+++ back-end/index.ts
@@ -1,10 +1,19 @@
 import express from "express";
 import cors from "cors";
 import multer from "multer";
+import { randomUUID } from "crypto";
+import fsPromises from "fs/promises";
+import fs from "fs";
+import unzipper from "unzipper";
+import { exec, spawn } from "child_process";
+import util from "util";
+
+const DEPLOYMENTS_DIR = process.env.DEPLOYMENTS_DIR ?? "/home/codersubham/deployments";
 
 const upload = multer({
-    dest: "uploadsXYZ/",
+    storage: multer.memoryStorage(),
 });
+const execPromise = util.promisify(exec);
 
 const app = express();
 app.use(express.json());
@@ -15,7 +24,7 @@ app.get("/health", (req, res) => {
     res.json({ status: "OK" });
 });
 
-app.post("/send-file", upload.single("project"), (req, res) => {
+app.post("/send-file", upload.single("project"), async (req, res) => {
     const file = req.file;
     if (!file?.originalname.endsWith(".zip")) {
         res.status(400).json({
@@ -23,7 +32,45 @@ app.post("/send-file", upload.single("project"), (req, res) => {
         });
         return;
     }
-    res.json({ message: "working" });
+
+    try {
+        const deploymentId = randomUUID();
+        const deploymentPath = DEPLOYMENTS_DIR + `/${deploymentId}`;
+        const zipPath = `${deploymentPath}/${req.file?.originalname ?? "project.zip"}`;
+        const sourcePath = `${deploymentPath}/source`;
+        console.log(req.file?.originalname);
+        //Creating the folder to put the zip file
+        await fsPromises.mkdir(deploymentPath, {
+            recursive: true,
+        });
+        //Zip file added
+        await fsPromises.writeFile(zipPath, req.file?.buffer!);
+        await fsPromises.mkdir(sourcePath);
+
+        await fs
+            .createReadStream(zipPath)
+            .pipe(unzipper.Extract({ path: sourcePath }))
+            .promise();
+
+        await execPromise("npm install", { cwd: sourcePath });
+
+        const child = spawn("npm", ["run", "dev"], {
+            cwd: sourcePath,
+        });
+
+        child.stdout.on("data", (data) => {
+            console.log(`[${deploymentId}]`, data.toString());
+        });
+
+        child.stderr.on("data", (data) => {
+            console.log(`[${deploymentId}]`, data.toString());
+        });
+
+        res.status(201).json({ message: "Successfully created the file" });
+    } catch (error) {
+        console.log("Error ->", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 });
 
 app.listen(3000, () => {
