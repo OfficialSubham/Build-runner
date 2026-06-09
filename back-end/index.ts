a7efafc33dc50cdda3bb471cadd6a8d80ea5f463
Subham Mondal <subhamoffi@gmail.com>
2026-06-09 17:52:50 +0530

    Created the get logs and status route

--- back-end/index.ts
+++ back-end/index.ts
@@ -6,7 +6,7 @@ import fsPromises from "fs/promises";
 import fs from "fs";
 import unzipper from "unzipper";
 import type { Response } from "express";
-import { createFile, runCommand, saveZip, startBuilding } from "./utils";
+import { createFile, saveZip, startBuilding, updateStatusJson } from "./utils";
 
 const DEPLOYMENTS_DIR = process.env.DEPLOYMENTS_DIR ?? "/home/codersubham/deployments";
 
@@ -46,8 +46,15 @@ app.post("/send-file", upload.single("project"), async (req, res) => {
 
     try {
         const deploymentId = randomUUID();
+        console.log("Deployment ID ->", deploymentId, "\n\n\n");
+
         //Creating the folder to put the zip file
         await createFile(DEPLOYMENTS_DIR + "/" + deploymentId);
+        //Creating Status json file as this create and update at the same time
+        await updateStatusJson(
+            `${DEPLOYMENTS_DIR}/${deploymentId}/status.json`,
+            "QUEUED",
+        );
         //Zip file added
         await saveZip(deploymentId, file);
 
@@ -59,6 +66,28 @@ app.post("/send-file", upload.single("project"), async (req, res) => {
     }
 });
 
+app.get("/deployments/:deploymentId/status", async (req, res) => {
+    const deploymentId = req.params.deploymentId;
+    const logsPath = `${DEPLOYMENTS_DIR}/${deploymentId}/logs.txt`;
+    const statusPath = `${DEPLOYMENTS_DIR}/${deploymentId}/status.json`;
+
+    try {
+        const logs = await fsPromises.readFile(logsPath, "utf-8");
+        const { status } = JSON.parse(await fsPromises.readFile(statusPath, "utf-8"));
+        res.json({
+            logs,
+            status,
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: null,
+            logs: null,
+            message: "Logs not found",
+        });
+    }
+});
+
+//This is actually SSE and this is a prototype project so I will not do this in this project
 app.get("/deployments/:deploymentId/logs/stream", (req, res) => {
     //Setting up the headers for streaming the text files
     res.setHeader("Content-Type", "text/event-stream");
