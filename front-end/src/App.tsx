4d286b5c8ed8107ae6a5bd8f8ea5047e236505d0
Subham Mondal <subhamoffi@gmail.com>
2026-06-09 17:46:52 +0530

    Implemented react router dom

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,38 +1,16 @@
-import { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Deployments from "./pages/deployments";
+import SendZip from "./pages/sendZip";
 
 function App() {
-    const URL = import.meta.env.VITE_API_URL;
-    const [file, setFile] = useState<File | null>(null);
-
-    const handleSubmit = async () => {
-        if (!file) return;
-        const formData = new FormData();
-        formData.append("project", file);
-        const res = await fetch(`${URL}/send-file`, {
-            method: "POST",
-            body: formData,
-        });
-        const data = await res.json();
-        console.log(data);
-    };
-
-    useEffect(() => {
-        console.log(file);
-    }, [file]);
-
     return (
         <>
-            <div>Hello hello</div>
-            <div>
-                <input
-                    type="file"
-                    accept=".zip"
-                    onChange={(e) => {
-                        setFile(e.target.files?.[0] ?? null);
-                    }}
-                />
-                <button onClick={handleSubmit}>Submit</button>
-            </div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" Component={SendZip} />
+                    <Route path="/deployments/:deploymentId" Component={Deployments} />
+                </Routes>
+            </BrowserRouter>
         </>
     );
 }
